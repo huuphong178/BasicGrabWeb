10ed4ccd33b43b24df9dae0c7d054785b7863874
huuphong178 <1512400@student.hcmus.edu.vn>
2018-10-28 09:57:36 +0700

    add api get post put request

--- repos/requestRepo.js
+++ repos/requestRepo.js
@@ -4,3 +4,54 @@ exports.loadAll = () => {
 	var sql = 'select * from request';
 	return db.load(sql);
 }
+
+exports.getRequest=id=>{
+    var sql=`select * from request where id=${id}`;
+    return db.excute(sql);
+}
+
+var GenerateID=function(){
+    var day = new Date();
+    var timenow = day.getTime();
+    return timenow;
+}
+var add=function(requestEntity){
+    // requestEntity = {
+    //     name: 'phong,
+    //     phone: '0123456789',
+    //     address: 'quan 11',
+    //     note: 'abc'
+    // }
+    var id=GenerateID();
+    var sql=`insert into request(id, name, phone, address, note) values(${id},'${requestEntity.name}',
+    '${requestEntity.phone}','${requestEntity.address}','${requestEntity.note}')`;
+    return db.excute(sql);
+}
+exports.add=requestEntity=>{
+    return add(requestEntity);
+};
+
+var Delete=function(id){
+    var sql=`delete from request where id= ${id}`;
+    return db.excute(sql);
+}
+exports.delete=id=>{
+    return Delete(id);
+}
+exports.update=requestEntity=>{
+    // requestEntity = {
+    //     id: 1540633245124,    
+    //     name: 'phong,
+    //     phone: '0123456789',
+    //     address: 'quan 11',
+    //     note: 'abc'
+    // }
+    return new Promise((resolve, reject)=>{
+        Delete(requestEntity.id)//delete request
+            .then(value=>{
+                return add(requestEntity); //add request
+        })
+        .then(value=> resolve(value))
+        .catch(err=>reject(err));
+    });
+}
